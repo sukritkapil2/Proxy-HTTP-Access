f9baca67c553b429dbbdcfa871fe382b93ff3908
Sukrit Kapil <sukrit.kapil2@gmail.com>
2021-02-22 10:21:11 +0530

    Final Version 1.0

--- http_proxy_download.c
+++ http_proxy_download.c
@@ -11,32 +11,83 @@
 #include <unistd.h>
 
 // this method searches for the first <img> tag in the main page and returns the logo
-void get_logo_url() {
+void download_logo(char* logo_name, char* proxy_server, char* proxy_port, char* username, char* password, char* auth_str, char* home_page) {
     char search_string[5000] = "<noscript><img";
     char word[5000];
+    char logo_url[5000], host_name[5000];
 
     FILE* fptr;
-    fptr = fopen("index.html", "r");
+    fptr = fopen(home_page, "r+b");
 
     while (fscanf(fptr, "%14s", word) != EOF) {
         if(strcmp(search_string, word) == 0) {
             fscanf(fptr, "%1000s", word);
-            char logo_url[5000];
             strncpy(logo_url, word+5, (int)(strlen(word) - 6));
-            printf("Logo URL: %s\n", logo_url);
-            return;
+            fclose(fptr);
+            break;
         }
     }
-    
+
+    char* temp_host = strstr(logo_url, "//");
+    char final_logo_url[5000] = "http://";
+    temp_host += 2;
+    strcat(final_logo_url, temp_host);
+
+    for(int i = 0;i < strlen(temp_host); i++) {
+        if(temp_host[i] == '/') break;
+        host_name[i] = temp_host[i];
+    }
+
+    int sock_fd;
+    struct sockaddr_in server;
+
+    fptr = fopen(logo_name, "w+b");
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_addr.s_addr = inet_addr(proxy_server);
+    server.sin_port = htons(atoi(proxy_port));
+    server.sin_family = AF_INET;
+
+    connect(sock_fd, (struct sockaddr *) &server, sizeof(server));
+
+    char message[8192] = "GET ";
+    strcat(message, final_logo_url);
+    strcat(message, " HTTP/1.1\r\nHost: ");
+    strcat(message, host_name);
+    strcat(message, "\r\nProxy-Authorization: Basic ");
+    strcat(message, auth_str);
+    strcat(message, "\r\nConnection: close\r\n\r\n");
+
+    send(sock_fd, message, strlen(message), 0);
+
+    char server_response[1024];
+    int recv_length;
+    int header_flag = 1;
+
+    while((recv_length = recv(sock_fd, server_response, 1024, 0)) > 0) {
+        if(header_flag == 1) {
+            char* after_remove_header = strstr(server_response, "\r\n\r\n");
+            after_remove_header += 4;
+            fwrite(after_remove_header, 1, strlen(after_remove_header)-4, fptr);
+        }
+        else fwrite(server_response, 1, recv_length, fptr);
+        fflush(fptr);
+        memset(server_response, 0, 1024);
+        header_flag = 0;
+    }
+
+    close(sock_fd);
     fclose(fptr);
+
+    return;
 }
 
 int main(int argc, char* argv[]) {
+    // declare the required variables
     int sock_fd;
     struct sockaddr_in server;
     FILE* fptr;
-
-    fptr = fopen("index.html", "w");
 
     // assign all the command-line arguments
     char* website = argv[1];
@@ -47,8 +98,11 @@ int main(int argc, char* argv[]) {
     char* home_page = argv[6];
     char* logo = argv[7];
 
-    if(logo == NULL) printf("\n[INFO] logo will not be downloaded\n");
+    fptr = fopen(home_page, "w+b");
 
+    if(logo == NULL) printf("\n[INFO] logo will not be downloaded!\n");
+
+    // convert the username:password to base64 encoded string
     char* auth_str = "Y3NmMzAzOmNzZjMwMw==";
     
     // create the socket
@@ -89,38 +143,25 @@ int main(int argc, char* argv[]) {
     }
     printf("\n[SUCCESS] request sent to the server\n\n[FETCHING] home page is being fetched...\n");
 
-    char* final_response;
-    final_response = (char *)malloc(sizeof(char) * 1024);
-    int total_size = 1024;
-    int current_size, old_size, total_received = 0;
-    int index = 0;
+    char server_response[1024];
+    int recv_length;
 
-    // fetch data in chunks from the server till no response is there
-    while((current_size = recv(sock_fd, final_response + index, 1024, 0)) > 0) {
-            index += current_size;
-            total_received += current_size;
-            old_size = total_size;
-            total_size += 1024;
-
-            char* temp_response = malloc(total_size);
-            memcpy(temp_response, final_response, old_size);
-            free(final_response);
-            final_response = temp_response;
+    while((recv_length = recv(sock_fd, server_response, 1024, 0)) > 0) {
+        fwrite(server_response, 1, recv_length, fptr);
+        fflush(fptr);
+        memset(server_response, 0, 1024);
     }
 
-    fprintf(fptr, "%s\n", final_response);
-    fflush(fptr);
-
-    if(logo != NULL) {
-        get_logo_url();
-    }
-
-    fclose(fptr);
-    close(sock_fd);
 
     printf("\n[SUCCESS] home page received from the server and saved to file\n");
 
-    free(final_response);
+    if(logo != NULL) {
+        printf("\n[FETCHING] logo is being downloaded...\n");
+        download_logo(logo, proxy_server, proxy_port, username, password, auth_str, home_page);
+        printf("\n[SUCCESS] logo downloaded successfully\n");
+    }
+
+    fclose(fptr);
 
     return 0;
 }
